037cd757f9113730b6ca30455895d35de135722c
MubbashirAhmed123 <mohdmubbashir71@gmail.com>
2025-05-08 18:30:54 +0530

    added server side rendering

--- server/public/sdk.js
+++ server/public/sdk.js
@@ -43,7 +43,7 @@ export async function collectUserDetails() {
             madeDetails,
         };
 
-        const res = await fetch('http://127.0.0.1:5000/details', {
+        const res = await fetch('https://web-sdk-eosin.vercel.app/details', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload),
